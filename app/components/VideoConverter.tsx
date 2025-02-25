aa580c2cb5711977cb9e31abe539cea34b2eca9c
[Ross Farrell] <[rossf@activeapparel.com.au]>
2025-02-25 11:30:48 +0800

    modified:   app/components/VideoConverter.tsx

--- app/components/VideoConverter.tsx
+++ app/components/VideoConverter.tsx
@@ -166,11 +166,11 @@ export default function VideoConverter() {
                     accept=".mp4,.webm"
                     onChange={handleFileChange}
                     className="block w-full text-sm text-gray-500
-                     file:mr-4 file:py-2 file:px-4
-                     file:rounded-md file:border-0
-                     file:text-sm file:font-semibold
-                     file:bg-blue-50 file:text-blue-700
-                     hover:file:bg-blue-100"
+                    file:mr-4 file:py-2 file:px-4
+                    file:rounded-md file:border-0
+                    file:text-sm file:font-semibold
+                    file:bg-blue-50 file:text-blue-700
+                    hover:file:bg-blue-100"
                 />
             </div>
 
