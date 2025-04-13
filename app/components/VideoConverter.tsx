5d35c5b60303252d34f4ab7f1ee7cdeaa4fed751
[Ross Farrell] <rossf@activeapparel.com.au>
2025-04-14 06:09:08 +0800

    modified:   app/components/VideoConverter.tsx

--- app/components/VideoConverter.tsx
+++ app/components/VideoConverter.tsx
@@ -5,7 +5,7 @@ import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 const ffmpeg = createFFmpeg({
     log: true,
-    corePath: "/esm/ffmpeg-core.js", // must point to ESM!
+    corePath: 'https://unpkg.com/@ffmpeg/core@0.12.9/dist/umd/ffmpeg-core.js'
 });
 
 let ffmpegLoaded = false;
